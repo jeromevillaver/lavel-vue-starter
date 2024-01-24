af94cd77029e2ba8911be8cb2db453c58f537411
jeromevillaver <jerome@lmf.ventures>
2024-01-24 02:55:59 +0000

    updated

--- app/Console/Commands/Repositories/GenerateRepositories.php
+++ app/Console/Commands/Repositories/GenerateRepositories.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\Repositories;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class GenerateRepositories extends Command
 {
@@ -38,6 +39,28 @@ class GenerateRepositories extends Command
         file_put_contents($interfacePath, $interfaceStub);
 
         $this->info($interfacePath . ' was created!');
+
+        // Ask the user for the type of request (admin or api)
+       $requestType = $this->ask('Is this request for admin or api?', 'Admin');
+       $requestType = in_array($requestType, ['Admin', 'Api']) ? $requestType : 'Admin';
+
+        if ($requestType !== 'Admin' && $requestType !== 'Api') {
+            $this->warn("Invalid input, using default value 'Admin'");
+        }
+
+        // Determine the folder path based on the selected type
+        $folderPath = (strcasecmp($requestType, 'admin') === 0) ? 'Admin' : 'Api';
+
+        // Run the make:request command
+        $command = "make:request {$folderPath}/" . $this->argument('model') . 'Request';
+        $this->info("Running command: $command");
+
+        try {
+            Artisan::call($command);
+            $this->info("Command completed successfully!");
+        } catch (\Exception $e) {
+            $this->error("Command failed with error: {$e->getMessage()}");
+        }
 
         return Command::SUCCESS;
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,15 +16,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    if (!Auth::check()) {
-    return redirect()->route('login');
-    }
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+     return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
